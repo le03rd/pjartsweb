dbc82bd3e8a67017ced48de6e9d8fac6379f8a25
Third <third.ordonez@gmail.com>
2017-07-23 20:36:24 +0800

    pull updates

--- public/js/factory.js
+++ public/js/factory.js
@@ -9,8 +9,7 @@ app.factory('fctry', function($http, $resource) {
 			      	animation_speed: 500,
 			      	stack_on_small: false,
 			      	navigation_arrows: true,
-			      	slide_number: true,
-			      	slide_number_text: 'of',
+			      	slide_number: false,
 			      	container_class: 'orbit-container',
 			      	stack_on_small_class: 'orbit-stack-on-small',
 			      	next_class: 'orbit-next',
@@ -21,7 +20,7 @@ app.factory('fctry', function($http, $resource) {
 			      	orbit_transition_class: 'orbit-transitioning',
 			      	bullets: false, 
 			      	circular: true,
-			      	timer: true, 
+			      	timer: false, 
 			      	swipe: true
 			  	}
 			}
@@ -33,13 +32,11 @@ app.factory('fctry', function($http, $resource) {
 					"image" : "public/assets/equipment-mimaki.png",
 					"description" : "Mimaki IJF-3042 Series Tabletop Printers can print on a variety of substances including plastics, metal, wood, leather and glass onto materials up to 12\" x 16\"." 
 				},
-
 				{
 					"equipment" : "Mimaki IJF-3042 Series Tabletop Flatbed UV Printer",
 					"image" : "public/assets/Peter-John-Brochure-Trifold-01.jpg",
 					"description" : "Mimaki IJF-3042 Series Tabletop Printers can print on a variety of substances including plastics, metal, wood, leather and glass onto materials up to 12\" x 16\"." 
 				},
-
 				{
 					"equipment" : "Mimaki IJF-3042 Series Tabletop Flatbed UV Printer",
 					"image" : "public/assets/Peter-John-Brochure-Trifold-02.jpg",
