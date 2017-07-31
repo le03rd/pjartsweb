8c7a9eb1b6637175859a65e646b1d0c25df17458
Third <third.ordonez@gmail.com>
2017-08-01 07:48:51 +0800

    update services, equipments

--- public/js/controller.js
+++ public/js/controller.js
@@ -8,9 +8,9 @@ app.controller('MainController', function($scope, fctry) {
 		list: [
 			{ title: 'Home', target: '#home', position: 'left' },
 			{ title: 'About', target: '#about', position: 'left' },
-			{ title: 'Services', target: '#services', position: 'left' },
-			{ title: 'Equipments', target: '#equipments', position: 'right' },
+			{ title: 'Equipments', target: '#equipments', position: 'left' },
 			{ title: 'Products', target: '#products', position: 'right' },
+			{ title: 'Services', target: '#services', position: 'right' },
 			{ title: 'Contact', target: '#contact', position: 'right' }
 		],
 		selected: null
@@ -40,7 +40,18 @@ app.controller('MainController', function($scope, fctry) {
 			{ caption: 'PVC ID', sub: 'Easily cstomize plastic PVC cards with any style of plastic cards', src: 'public/assets/featured/product-pvcid.jpg' },
 			{ caption: 'Phone Cases', sub: 'Express yourself with original phone cases and decal skin designs made by you', src: 'public/assets/featured/product-phonecases.jpg' },
 		]
-	}
+	};
+	$scope.services = [
+		{ service: 'Acrylic Medals', src: 'public/assets/services/acrylic-medal.png', anim: 'fade-left' },
+		{ service: 'Acrylic Miniature', src: 'public/assets/services/acrylic-miniture.png', anim: 'fade' },
+		{ service: 'Acrylic Souvenir', src: 'public/assets/services/acrylic-souvenir.png', anim: 'fade-right' },
+		{ service: 'Acrylic Table Sign', src: 'public/assets/services/acrylic-table-sign.png', anim: 'fade-left' },
+		{ service: 'Acrylic Trophy', src: 'public/assets/services/acrylic-trophy.png', anim: 'fade' },
+		{ service: 'Keychain', src: 'public/assets/services/keychain.png', anim: 'fade-right' },
+		{ service: 'UV Print Phonecase', src: 'public/assets/services/uv-print-phone-case.png', anim: 'fade-left' },
+		{ service: 'PVC Signages', src: 'public/assets/services/pvc-signages.png', anim: 'fade' },
+		{ service: 'PVC ID', src: 'public/assets/services/pvc-id.png', anim: 'fade-right' },
+	];
 
 	return $scope.init();
 });
--- public/js/factory.js
+++ public/js/factory.js
@@ -4,16 +4,19 @@ app.factory('fctry', function($http, $resource) {
 			{
 				"equipment" : "Mimaki IJF-3042 Series Tabletop Flatbed UV Printer",
 				"image" : "public/assets/equipment-mimaki.jpg",
+				"color": "blue",
 				"description" : "Mimaki IJF-3042 Series Tabletop Printers can print on a variety of substances including plastics, metal, wood, leather and glass onto materials up to 12\" x 16\"." 
 			},
 			{
 				"equipment" : "Challenger FY 3208-L 10 FT Large Format Printer",
 				"image" : "public/assets/equipment-challenger.jpg",
+				"color": "green",
 				"description" : "Mostly used for printing billboards and generally have the capability of printing between 60 and 160 square meters per hour." 
 			},
 			{
 				"equipment" : "Roland Soljet Pro III XC-540 Ink Jet Printer / Cutter",
 				"image" : "public/assets/equipment-roland.jpg",
+				"color": "red",
 				"description" : "The XC-540 is based on Roland's innovative print/cut technology, which delivers a seamless workflow for the production of complex sign graphics." 
 			}
 		]
